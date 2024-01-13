0329a9412ffcfb677f223fb4b5892132a02d82a9
Roman <roma.makhunov@gmail.com>
2024-01-13 09:30:20 +0300

    added CustomUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class CustomUser(AbstractUser):
+    phone = models.CharField(max_length=15, null=True, blank=True)
+
+    class Meta:
+        db_table = 'user'
 
 # Create your models here.
